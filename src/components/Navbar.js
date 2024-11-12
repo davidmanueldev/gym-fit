0a40f0371cbb85e0fb076b3de535f32080a4d526
David Manuel <davidmanuel.dev@gmail.com>
2024-11-12 19:27:05 -0400

    Middleware added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 function Navbar() {
 
   const { data: session } = useSession()
-  console.log(session)
+  // console.log(session)
   return (
     <nav className='bg-slate-900 flex items-center py-3 justify-between px-24 text-white'>
       <Link href="/">
@@ -19,20 +19,23 @@ function Navbar() {
             Dashboard
           </Link>
           <p>{session.user.name} {session.user.email}</p>
-          <img src={session.user.image} alt='i' className='w-10 h-10 rounded-full cursor-pointer' />
+          <img src={session.user.image} alt='imagen del perfil' className='w-10 h-10 rounded-full cursor-pointer' />
 
-          <button onClick={() => {signOut()}}
-            className='bg-sky-400 px-3 py-2 rounded'>
-            Cerrar Sesión
+          <button onClick={async () => {
+            await signOut({
+              callbackUrl: '/'
+            })
+          }}>
+            Sign Out
           </button>
-
         </div>
       ) : (
-        <button onClick={() => signIn()} className='bg-sky-400 px-3 py-2 rounded'>
-          Iniciar Sesión
+        <button onClick={async () => {
+          await signIn(null, { callbackUrl: '/dashboard' })
+        }}>
+          Sign In
         </button>
       )}
-
     </nav>
   )
 }
